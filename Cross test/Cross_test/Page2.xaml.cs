e4e3ef7f1eda7566a203ecca568471da6bc20129
Aabid Umer <aabid@whizsoftwares.in>
2016-12-26 12:48:53 +0530

    Add project files.

---
+++ Cross test/Cross_test/Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Cross_test
+{
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            string text = MainEntry.Text;
+
+            MainLabel.Text = "Hello:" + text;
+        }
+	}
+}
